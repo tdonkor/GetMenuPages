bd34859127c0a0c64dff9e039458665bf5b01ee3
tdonkor <tdonkor@gmail.com>
2019-05-23 16:36:40 +0100

    Add project files.

---
+++ GetMenuPages/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMenuPages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine();
+                Log.Info("\nMenu Pages Started.");
+                GetMenuPages menuPages = new GetMenuPages();
+
+                var guid = Guid.NewGuid().ToString();
+                Log.Info($"Guid Value set at : {guid}");
+
+                menuPages.GetMenuPagesInformation(guid);
+                Log.Info("Menu Pages Finished.\n");
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error" + ex);
+            }
+
+        }
+    }
+}
---
+++ GetMenuPages/RequestDetails.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMenuPages
+{
+    public class RequestDetails
+    {
+        public void HeaderInformation(out RestClient client, out RestRequest request)
+        {
+
+            client = new RestClient(ConfigurationManager.AppSettings["url"]);
+            request = new RestRequest(Method.POST);
+
+
+            //header items
+            request.AddHeader("cache-control", ConfigurationManager.AppSettings["cacheValue"]);
+            request.AddHeader("x-auth-brandtoken", ConfigurationManager.AppSettings["tokenValue"]);
+            request.AddHeader("Accept", ConfigurationManager.AppSettings["acceptValue"]);
+            request.AddHeader("Content-Type", ConfigurationManager.AppSettings["contentTypeValue"]);
+        }
+    }
+}
